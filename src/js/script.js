f5e58e1829ee5357b9b7ba1925077df2c6f43b14
MarinaMyz <marynakravchenko77@gmail.com>
2023-12-16 13:09:56 -0800

    hw3 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const firstOperand = Number(prompt('Please enter first number'));
+const secondOperand = Number(prompt('Please enter second number'));
+
+const plusResult = firstOperand + secondOperand;
+console.log(`Result: ${firstOperand} + ${secondOperand} = ${plusResult}`); // result: 2 + 3 = 5
+//console.log('Result: ' + firstOperand + ' + ' + secondOperand + ' = ' + plusResult); // result: 2 + 3 = 5
+
+const minusResult = firstOperand - secondOperand;
+console.log(`Result: ${firstOperand} - ${secondOperand} = ${minusResult}`);
+
+const multiolicationResult = firstOperand * secondOperand;
+console.log(`Result: ${firstOperand} * ${secondOperand} = ${multiolicationResult}`);
+
+const divisionResult = firstOperand / secondOperand;
+console.log(`Result: ${firstOperand} / ${secondOperand} = ${divisionResult} `);
