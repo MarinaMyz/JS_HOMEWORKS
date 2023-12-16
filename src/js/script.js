8277960883c1808a8a2cbd896d2ab442b552029a
MarinaMyz <marynakravchenko77@gmail.com>
2023-12-16 12:28:44 -0800

    hw2 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const name = 'John';
+const admin = name;
+alert(admin);
+
+//task2
+const ourPlanet = 'Earth';
+const visitor = 'John';
+
+//task3
+const stringPlusNumber = '10'+ 10;
+console.log(typeof stringPlusNumber);
+//В JavaScript числа, з’єднані з рядком, перетворюються в рядок, а не кількість. Так відбувається, тому що за замовчуванням JavaScript виконує код зліва направо.JavaScript спочатку виробляє конкатенацию тексту з змінної, а не додавання змінних.
+
