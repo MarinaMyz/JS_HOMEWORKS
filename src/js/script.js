1358e64bd7a3fb57fa68d2ac3ed06a257fd6ad83
MarinaMyz <marynakravchenko77@gmail.com>
2023-12-20 20:48:00 +0200

    hw4 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,10 @@
 'use strict';
-//const userName = prompt('Enter your name');
-//const age = prompt("How old are you?");
-//const question = prompt('Where are you from?')
-//alert('Hello ' + userName + ', You ' + age + ', You from '+ question);
+const userName = prompt('Enter your name');
+const age = prompt("How old are you?");
+const question = prompt('Where are you from?')
+alert('Hello ' + userName + ', You ' + age + ', You from '+ question);
 
-
+//task 2
 const number = (prompt('Please enter a five digit number'));
 const array = number.split("").join(" ");
 console.log(array);
