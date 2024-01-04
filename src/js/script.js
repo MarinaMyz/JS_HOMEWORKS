d3639f274df4d381dc950f6f8380ba3bb41c5549
MarinaMyz <marynakravchenko77@gmail.com>
2024-01-04 21:30:05 +0200

    hw15 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,7 @@
 'use strict';
+const num1 = Number (prompt('Please write first number'));
+const num2 = Number (prompt('Please write second number'));
+const num3 = Number (prompt('Please write third number'));
+const plusResult = num1 + num2 + num3;
+const average = plusResult / 3;
+alert (average);
