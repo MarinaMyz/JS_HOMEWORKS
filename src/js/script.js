706151c358555bc535ef4e44f57376b021bb1cbe
MarinaMyz <marynakravchenko77@gmail.com>
2024-01-04 23:47:45 +0200

    hw12 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
 'use strict';
-const num1 = Number(prompt('Please write first number'));
-const num2 = Number(prompt('Please write second number'));
+const num1 = Number( prompt ('Please write first number'));
+const num2 = Number( prompt ('Please write second number'));
 const plusResult = num1 + num2;
-alert ('Result: ' + num1 + '+' + num2 + '=' + plusResult);
 const minusResult = num1 - num2;
-alert ('Result: ' + num1 + '-' + num2 + '=' + minusResult);
-const multiolicationResult = num1 * num2;
-alert ('Result: ' + num1 + '*' + num2 + '=' + multiolicationResult);
+const multiplicationResult = num1 * num2;
 const divisionResult = num1 / num2;
-alert ('Result: ' + num1 + '/' + num2 + '=' + divisionResult);
+alert('Result: ' + num1 + ' + ' + num2 + ' = ' + plusResult + '\n' +
+    num1 + ' - ' + num2 + ' = ' + minusResult + '\n' +
+    num1 + ' / ' + num2 + ' = ' + divisionResult + '\n' +
+    num1 + ' * ' + num2 + ' = ' + multiplicationResult);
