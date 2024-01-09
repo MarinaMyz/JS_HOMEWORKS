5bbc2d5e83fb05e8e771ac617ac83929eba17af1
MarinaMyz <marynakravchenko77@gmail.com>
2024-01-09 10:14:06 +0200

    hw13 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+let operation = prompt('What do you want to earn (add, sub, mult, div)?');
+let num1 = Number(prompt('Please write first number'));
+let num2 = Number(prompt('Please write second number'));
+let result;
+
+if(operation === 'add') {
+    result = num1 + num2;
+    alert(num1 + ' ' + '+' + ' ' + num2 + ' = ' + result);
+} else if (operation === 'sub'){
+    result = num1 - num2;
+    alert(num1 + ' ' + '-' + ' ' + num2 + ' = ' + result);
+} else if (operation === 'mult') {
+    result = num1 * num2;
+    alert(num1 + ' ' + '*' + ' ' + num2 + ' = ' + result);
+} else if (operation === 'div') {
+    result = num1 / num2;
+    alert(num1 + ' ' + '/' + ' ' + num2 + ' = ' + result);
+}
+
