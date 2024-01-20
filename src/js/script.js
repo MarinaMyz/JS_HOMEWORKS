513f26bed8024622ac658f2fd842564a072c1e21
MarinaMyz <marynakravchenko77@gmail.com>
2024-01-20 16:06:24 +0200

    hw28 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+function padString(string, number, symbol = '*', right = true ) {
+    if (!string || !number) {
+        return 'Error';
+    }
+    if(typeof symbol !== 'string' && symbol.length !== 1){
+        return 'Error';
+    }
+    if (string.length === number) {
+        return string;
+    }
+    if (string.length > number) {
+        return string.substring(0, number);
+    }
+    if (right) {
+        return string.padEnd(number, symbol);
+    }
+    return string.padStart(number, symbol);
+}
+
+console.log(padString('bye', 8, '@'));
