214ae61d7f53d55aab8fbcd7172f32cfd35e4915
MarinaMyz <marynakravchenko77@gmail.com>
2024-01-20 14:34:44 +0200

    hw27 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+const firstNumber = Number(prompt('Please write number'));
+const degree = Number(prompt('to what power to raise the number'));
+console.log(firstNumber, degree);
+function numberToDegree(number, degree = 1){
+    if (isNaN(number) || isNaN(degree)){
+        return 'Error';
+    }
+    return number ** degree;
+}
+const result = numberToDegree(firstNumber, degree);
+alert(result);
+
+
