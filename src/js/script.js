f38a10d4cd3d0c1a997fa2672986fe1a57bab9ff
MarinaMyz <marynakravchenko77@gmail.com>
2024-01-20 16:34:26 +0200

    hw29 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+const arr = [1, 2, 3, -1, -2, -3];
+function positiveNumbers(arr) {
+    if(arr.length === 0){
+        return 'empty array';
+    }
+    const result = [];
+    for(let i= 0; i< arr.length; i++){
+      if(arr[i] > 0) {
+          result.push(arr[i]);
+      }
+    }
+    if(result.length !== 0){
+        return result;
+    }else {
+        return null;
+    }
+}
+console.log(positiveNumbers(arr));
