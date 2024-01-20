dcd0e031a5eecc01455cebf10ea85539911c6dad
MarinaMyz <marynakravchenko77@gmail.com>
2024-01-20 15:15:18 +0200

    hw26 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+function generateKey(length, characters) {
+    let result = '';
+    const charactersLength = characters.length;
+
+    for (let i= 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+console.log(generateKey(8, characters));
