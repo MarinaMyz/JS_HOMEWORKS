a09943a1bbfeefb75168326d19eef47687ada10d
MarinaMyz <marynakravchenko77@gmail.com>
2024-01-28 19:47:21 +0200

    hw30 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,8 @@
 'use strict';
 
+function calculatePrice () {
+    const potatoPerLiter = 75 * 4;
+    const overallPotatoes= potatoPerLiter * 48 / 1000;
+    return overallPotatoes * 13;
+}
+alert(Math.ceil(calculatePrice()));
