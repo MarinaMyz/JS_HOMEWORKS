03f66b850a86dab87ce20782ef4ca9a82828ae05
MarinaMyz <marynakravchenko77@gmail.com>
2024-01-28 19:04:06 +0200

    hw24 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const lowestDivisor = Number(prompt('Please write number'));
+
+if (isNaN(lowestDivisor)) {
+    alert("Ви ввели не число!");
+} else if (lowestDivisor < 2) {
+    alert("Число повинно бути більшим або дорівнювати 2");
+} else {
+    for (let i = 2; i <= lowestDivisor; i++) {
+        if (lowestDivisor % i === 0) {
+            alert(`Мінімальний дільник числа ${lowestDivisor} є ${i}`);
+            break;
+        }
+    }
+}
