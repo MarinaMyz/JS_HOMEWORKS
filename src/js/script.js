1eca38c239df38a55fb9f9a07c45f89ceb648ad4
MarinaMyz <marynakravchenko77@gmail.com>
2024-01-29 16:36:35 +0200

    hw34 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,28 @@
 'use strict';
+const users = function () {
+    const sayHi = function () {
+        console.log('hello')
+    }
+    return function (name, age) {
+        const lastName = 'Doe';
 
+        return {
+            name,
+            age,
+            lastName,
+            sayHi
+
+        }
+    }
+}
+const createUser = users();
+
+const user = createUser( 'Mike',  26);
+const user2 = createUser(  'Anna',  28);
+const user3 = createUser( 'John',  24);
+
+console.log(user,user2);
+console.log(user.sayHi === user2.sayHi);
+
+user.sayHi()
+user2.sayHi()
