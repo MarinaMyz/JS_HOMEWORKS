5cbc42dc758bae9a71f529af4cf8ff68366bd147
MarinaMyz <marynakravchenko77@gmail.com>
2024-01-29 17:55:00 +0200

    hw31 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,22 @@
 'use strict';
 
+const uniqueNumbers = [];
+
+function randomNumber() {
+    if (uniqueNumbers.length >= 100) {
+        return null;
+    }
+
+    let generatedNumber;
+    do {
+        generatedNumber = Math.floor(Math.random() * 100) + 1;
+    } while (uniqueNumbers.includes(generatedNumber));
+
+    uniqueNumbers.push(generatedNumber);
+    return generatedNumber;
+}
+
+for (let i = 1; i <= 100; i++) {
+    console.log(randomNumber());
+}
+
