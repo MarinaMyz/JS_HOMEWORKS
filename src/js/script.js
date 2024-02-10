4f92fcfc72dd4e18b77b455ad0931f3da8a3e563
MarinaMyz <marynakravchenko77@gmail.com>
2024-02-10 16:24:32 +0200

    hw37 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,20 @@
 'use strict';
 
+function parseUrl(url) {
+    const parser = document.createElement('a');
+    parser.href = url;
+
+    return {
+        protocol: parser.protocol,
+        host: parser.host,
+        path: parser.pathname,
+        hash: parser.hash
+    };
+}
+const url = 'https://example.com:8080/path/name?query=123#hash';
+const parsedUrl = parseUrl(url);
+
+console.log(parsedUrl.protocol);
+console.log(parsedUrl.host);
+console.log(parsedUrl.path);
+console.log(parsedUrl.hash);
