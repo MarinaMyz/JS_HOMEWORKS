0537b42a7bc99f2e5a59cd57a7bd3752e48dbb76
MarinaMyz <marynakravchenko77@gmail.com>
2024-02-17 20:04:22 +0200

    hw45 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+let ladder = {
+    step: 0,
+    up: function() {
+        this.step++;
+        return this; // return the object itself to allow chaining
+    },
+    down: function() {
+        this.step--;
+        return this; // return the object itself to allow chaining
+    },
+    showStep: function() {
+        alert(this.step);
+        return this; // return the object itself to allow chaining
+    }
+};
+console.log(ladder.up().up().down().showStep());
