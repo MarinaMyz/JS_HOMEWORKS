df70d6cf33a65454cbd086cd7c4bb0190ea9fa47
MarinaMyz <marynakravchenko77@gmail.com>
2024-02-18 15:47:26 +0200

    hw20 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,21 @@
 'use strict';
 
+
+const userAge = prompt("Введіть число:");
+const number = parseInt(userAge);
+
+if (isNaN(number)) {
+    alert("Будь ласка, введіть число.");
+} else {
+    let result;
+
+    if (number === 1) {
+        result = "рік";
+    } else if (number >= 2 && number <= 4) {
+        result = "роки";
+    } else {
+        result = "років";
+    }
+
+    alert(`${number} ${result}`);
+}
