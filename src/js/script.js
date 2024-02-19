961559fc387aef802d303762882069eb5855e033
MarinaMyz <marynakravchenko77@gmail.com>
2024-02-19 12:35:51 +0200

    hw43 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,26 @@
 'use strict';
 
+//1
+function getFactorial(n) {
+    return (n != 1) ? n * getFactorial(n - 1) : 1;
+}
+console.log( getFactorial(3) );
+
+//2
+function pow(num, degree) {
+    if(degree == 1) {
+        return num;
+    } else {
+        return num * pow(num, degree - 1);
+    }
+}
+console.log(pow(5,2));
+
+//3
+function sum(a,b) {
+    if(b === 0) {
+        return a;
+    }
+    return sum(++a, --b);
+}
+console.log(sum(4,6));
