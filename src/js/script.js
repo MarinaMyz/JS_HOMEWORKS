c757f1c2e200995db00ba989cfcedfe404b61faa
MarinaMyz <marynakravchenko77@gmail.com>
2024-02-26 13:48:34 +0200

    hw40 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,15 @@
 'use strict';
 
+function myReduce(arr, callback, initialValue) {
+    let accumulator = initialValue === undefined ? arr[0] : initialValue;
+
+    for (let i = initialValue === undefined ? 1 : 0; i < arr.length; i++) {
+        accumulator = callback(accumulator, arr[i]);
+    }
+    return accumulator;
+}
+
+const arr = [2, 4, 6, 8, 10];
+const sum = myReduce(arr, (acc, curr) => acc + curr, 0);
+
+console.log(sum);
