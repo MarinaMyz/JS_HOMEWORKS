b8650b58dd06c9cfb6fc6a5d39412f2af74fd699
MarinaMyz <marynakravchenko77@gmail.com>
2024-02-29 20:15:15 +0200

    hw52 completed

--- src/js/solution.js
+++ src/js/solution.js
@@ -4,14 +4,14 @@ const ulElement = document.getElementById('ulId');
 const liElements = ulElement.getElementsByTagName('li');
 
 
-for (const li of liElements) {
+for (let li of liElements) {
     console.log(li.textContent);
 }
 console.log(liElements.length);
 
 
 const textArray = [];
-for (const li of liElements) {
+for (let li of liElements) {
     textArray.push(li.textContent);
 }
 console.log(textArray);
