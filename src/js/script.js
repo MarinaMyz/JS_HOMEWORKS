b62347ce7cfcee15ea83d8dbe6a666155fd9e65f
MarinaMyz <marynakravchenko77@gmail.com>
2024-03-09 19:20:47 +0200

    hw51 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,20 @@
 'use strict';
 
+function parseJSON(jsonString) {
+    try {
+        const jsonObject = JSON.parse(jsonString);
+        return jsonObject;
+    } catch (error) {
+        console.error('Error parsing JSON:', error);
+        return null;
+    }
+}
+
+const jsonString = '{"name": "John", "age": 30}';
+const jsonObject = parseJSON(jsonString);
+
+if (jsonObject) {
+    console.log( jsonObject);
+} else {
+    console.log('Invalid JSON string');
+}
