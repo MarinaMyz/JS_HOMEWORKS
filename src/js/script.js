2d18eccaa459254f2d2dde4cbb270d54b43e96f3
MarinaMyz <marynakravchenko77@gmail.com>
2024-03-09 18:22:09 +0200

    hw46 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,20 @@
 'use strict';
 
+const user = {
+    name: 'Marina',
+    age: 25,
+    language: 'ukrainian',
+}
+
+const printName = function (prefix) {
+    console.log(this, prefix);
+    console.log(prefix + ' ' + this.name);
+
+}
+function myApply(func, ctx, args) {
+  ctx.printName = func;
+  ctx.printName(args, ...args);
+  delete ctx.printName;
+}
+
+myApply(printName, user, ['Dear']);
