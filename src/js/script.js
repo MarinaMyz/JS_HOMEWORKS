d9912368e4f8fe3efc62ffa50011ed6cfaf306ae
MarinaMyz <marynakravchenko77@gmail.com>
2024-03-09 13:20:53 +0200

    hw54 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,21 @@
 'use strict';
+function createTable() {
+    const table = document.createElement('table');
 
+    for (let i = 1; i <= 10; i++) {
+        const row = document.createElement('tr');
+
+        for (let j = 1; j <= 10; j++) {
+            const cell = document.createElement('td');
+            const cellValue = (i - 1) * 10 + j;
+            const cellText = document.createTextNode(cellValue);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+
+        table.appendChild(row);
+    }
+
+    document.body.appendChild(table);
+}
+createTable();
